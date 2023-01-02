56728818590ea7c20df82852df9cf472c11d9341
Robson Trasel <robson.trasel@gmail.com>
2023-01-01 23:09:52 -0300

    chore: add text to be a desc on categories command

--- src/index.ts
+++ src/index.ts
@@ -9,10 +9,21 @@ const bot = new Telegraf(token)
 
 
 const modules = fs.readdirSync(join(__dirname, 'modules'))
+const actions = fs.readdirSync(join(__dirname, 'actions'))
 for(const module of modules)
 {
   const command = require(join(__dirname, 'modules', module))
-  bot.command(command.name, command.handler)
+  bot.command(command.name, ctx => {
+    ctx.state.command = command
+    ctx.state.actions = command.actions
+    ctx.reply(command.desc)
+  })
+}
+
+for(const action of actions)
+{
+  const actionFile = require(join(__dirname, 'actions', action))
+  bot.action(actionFile.name, actionFile.handler)
 }
 
 bot.launch()
--- src/modules/categories/index.ts
+++ src/modules/categories/index.ts
@@ -2,5 +2,5 @@ import { Context } from "telegraf";
 
 export const name = "categorias"
 export const handler = (ctx: Context) => {
-  
+  ctx.answerCbQuery("Aqui estão as categorias disponiveis")
 }
