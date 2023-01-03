fdefb50f1870697c3dc6c2d953b09870e8f72d54
RobsonTrasel <robson.trasel@gmail.com>
2023-01-03 13:42:01 -0300

    chore: message changed

--- src/modules/start/index.ts
+++ src/modules/start/index.ts
@@ -1,21 +1,8 @@
 import { Context } from 'telegraf'
 
 export const name = 'start'
+export const desc = "Exibe a mensagem de boas-vindas do bot"
 export const handler = (ctx: Context) => {
-  ctx.replyWithMarkdownV2(
-    "Olá, seja bem vindo ao RibbieBOT, escolha uma opção",
-    {
-      reply_markup: {
-        inline_keyboard: [[{
-          text: "Categorias",
-          callback_data: 'categories'
-        },
-        {
-          text: "Github",
-          callback_data: "link"
-        }
-      ]]
-      }
-    }
-  )
+  ctx.reply('Olá! Eu sou o RibbiePDFBot, um bot que auxilia na busca por PDFs. Digite /help parar os comandos disponiveis.')
 }
+
