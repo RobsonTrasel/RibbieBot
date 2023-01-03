11205b49114327316a67f222c703816210232205
RobsonTrasel <robson.trasel@gmail.com>
2023-01-03 13:45:37 -0300

    chore: moved the command handler to another archive

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,13 @@
 import dotenv from 'dotenv'
 dotenv.config()
 import { Telegraf } from 'telegraf'
-import * as fs from 'fs'
+import { registerCommands } from './utils/command-handler'
 import { join } from 'path'
 
 const token: string = process.env.TOKEN || ""
 const bot = new Telegraf(token)
-const modules = fs.readdirSync(join(__dirname, 'modules'))
-const actions = fs.readdirSync(join(__dirname, 'actions'))
 
-for(const module of modules)
-{
-  const command = require(join(__dirname, 'modules', module))
-  bot.command(command.name, ctx => {
-    ctx.state.command = command
-    ctx.state.actions = command.actions
-    ctx.reply(command.desc)
-  })
-}
-
-for(const action of actions)
-{
-  const actionFile = require(join(__dirname, 'actions', action))
-  bot.action(actionFile.name, actionFile.handler)
-}
+registerCommands(bot, join(__dirname, 'modules'))
 
 
 bot.launch()
